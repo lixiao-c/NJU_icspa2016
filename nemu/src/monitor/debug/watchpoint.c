cd44de68fab74263cf30a273023d1eb29a5ea218
tracer-ics2016 <tracer@njuics.org>
2018-10-27 19:54:13 +0800

    >  compile NEMU 151220000 clx Linux clx-VirtualBox 4.15.0-38-generic #41-Ubuntu SMP Wed Oct 10 10:59:38 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  19:54:13 up  6:51,  1 user,  load average: 0.01, 0.08, 0.05 f2446ae1c01683ffe30a0555a97fa2127ec4fc6e

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -19,5 +19,34 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_wp()
+{
+	if(free_==NULL)
+		assert(0);
+	WP *temp=free_;
+	free_=free_->next;
+	temp->next=NULL;
+	return temp;
+}
 
+void free_wp(WP *wp)
+{
+	if(head==wp)
+		head=NULL;
+	else{	
+		WP *temp=head;
+		while(temp->next!=wp)
+		{
+			temp=temp->next;
+		}
+		temp->next=wp->next;
+		wp->next=NULL;
+	}
+	WP *free_temp=free_;
+	while(free_temp->next!=NULL)
+	{
+		free_temp=free_temp->next;
+	}
+	free_temp->next=wp;
+}
 
