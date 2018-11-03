7763fc3317f008ab76b2b263303250ae6a59a83f
tracer-ics2016 <tracer@njuics.org>
2018-11-03 20:50:36 +0800

    >  compile NEMU 151220000 clx Linux clx-VirtualBox 4.15.0-38-generic #41-Ubuntu SMP Wed Oct 10 10:59:38 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:50:36 up  1:52,  1 user,  load average: 0.02, 0.21, 0.30 e7397b0926d960dfe6b95e8f4c71c102962b78f

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -3,8 +3,14 @@
 #define instr call
 
 static void do_execute() {
-	cpu.eip+=op_src->val;
-	print_asm("call $0x%x",cpu.eip);
+	if(instr_fetch(cpu.eip,1)==0xe8){
+		cpu.eip+=op_src->val;
+		print_asm("call $0x%x",cpu.eip);
+	}
+	else{
+		cpu.eip=op_src->val;
+		print_asm("call $0x%x",cpu.eip);
+	}
 }
 make_instr_helper(i)
 make_instr_helper(rm)
