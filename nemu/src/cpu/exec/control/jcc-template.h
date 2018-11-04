a83c90fbe0a560c9665cf1b4a1d0bcfe6ea8f9f3
tracer-ics2016 <tracer@njuics.org>
2018-11-04 13:52:20 +0800

    >  compile NEMU 151220000 clx Linux clx-VirtualBox 4.15.0-38-generic #41-Ubuntu SMP Wed Oct 10 10:59:38 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  13:52:20 up  3:16,  1 user,  load average: 0.10, 0.17, 0.11 50f3cdcd69494ffbd9dfe50cc1bf5f1e719896bb

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -5,9 +5,7 @@
 static void do_execute() {
 	DATA_TYPE tail=op_src->val;
 	uint32_t temp=cpu.eip+tail;
-	if(DATA_BYTE==1)
-		temp&=0xff;
-	else if(DATA_BYTE==2)
+	if(DATA_BYTE==2)
 		temp&=0xffff;
 	if(cpu.eflags.zf==1)
 		cpu.eip=temp;
